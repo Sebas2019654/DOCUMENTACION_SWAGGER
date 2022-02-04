67cd21651693683439b7aa1a62237618f39b132a
Sebas2019654 <89703787+Sebas2019654@users.noreply.github.com>
2022-02-04 14:49:58 -0500

    swagger

---
+++ Trabajo/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+insert into empleados(id,nombre,apellido,ci,fecha_n,sexo,salario,superci,dno) values (1,'Victoria','Cajas','0106271836','1995-02-07','F',100,666,990);
+insert into productos(id_Producto,nombre,descripcion,costo,stock) values (1,'Magnun','Helado',150,5);
+insert into vendedor(id_vendedor,nombre,apellido,direccion,telefono,email) values (1,'Sandra', 'Mejia', 'Baños','0999041505','sandra@hotmail.com');
+insert into clientes(id,nombre,apellido,email,create_at) values (1,'Carmen', 'Tacuri', 'tacurijuanita@hotmail.com','1974-01-04');
+insert into clientes(id,nombre,apellido,email,create_at) values (2,'Patricia', 'Tacuri', 'pa@hotmail.com','1975-01-04');
+insert into clientes(id,nombre,apellido,email,create_at) values (3,'Luis', 'Villa', 'villa@hotmail.com','1976-01-04');
+insert into clientes(id,nombre,apellido,email,create_at) values (4,'Tomas', 'Erazo', 'tomas@hotmail.com','1964-01-04');
+insert into clientes(id,nombre,apellido,email,create_at) values (5,'Daniela', 'Rosero', 'danny@hotmail.com','1992-01-04');
